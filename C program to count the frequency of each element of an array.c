22b57360b8c3b32277b1f312b6a35efe839b45ed
Ramachandraa PS <62879218+positivehacker2005@users.noreply.github.com>
2023-03-29 20:46:02 +0530

    Create C program to count the frequency of each element of an array.c

---
+++ C program to count the frequency of each element of an array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+  int arr[100], freq[100], size, i, j, count;
+  printf("Enter size of the array: ");
+  scanf("%d", &size);
+  printf("Enter elements in the array: ");
+  for(i=0; i<size; i++) {
+    scanf("%d", &arr[i]);
+    freq[i] = -1;
+  }
+  for(i=0; i<size; i++) {
+    count = 1;
+    for(j=i+1; j<size; j++) {
+      if(arr[i] == arr[j]) {
+        count++;
+        freq[j] = 0;
+      }
+    }
+    if(freq[i] != 0) {
+      freq[i] = count;
+    }
+  }
+  printf("Frequency of each element in the array:\n");
+  for(i=0; i<size; i++) {
+    if(freq[i] != 0) {
+      printf("%d occurs %d times\n", arr[i], freq[i]);
+    }
+  }
+  return 0;
+}
